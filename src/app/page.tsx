ea5b592ccf9fd0627e12439deae4778773ec9f47
Mownica_webdev <mownica.p@gmail.com>
2024-11-18 00:29:00 +0530

    next.js-14 project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,13 +30,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
+            <h1>next 14 app</h1>
             Deploy now
           </a>
           <a
